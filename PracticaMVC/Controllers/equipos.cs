a07f371deebdd67a8b7b9e5722663c80dcba61a2
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-04-15 09:53:15 -0600

    Introduccion de formulario Equipos

---
+++ PracticaMVC/Controllers/equipos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaMVC.Controllers
+{
+    public class equipos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
